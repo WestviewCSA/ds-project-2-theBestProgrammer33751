f657fe5fa335c240b9279b69654ad640e5e87f67
1924324 <1924324@01-18106.pusd.dom>
2025-03-11 14:11:02 -0700

    Did some on the Map class

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -3,6 +3,13 @@ public class Map {
 
 	private Tile[][] map; //is 2D enough
 	
+	private int rows;
+	private int columns;
+	
+	public Map(int rows, int columsn) {
+		this.rows = rows;
+		this.columns = columns;
+	}
 	
 	
 }
