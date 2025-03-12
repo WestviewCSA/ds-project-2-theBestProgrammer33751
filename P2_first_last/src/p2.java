7b99b7172a654947bc9037dc08dbdae9b846cc0b
1924324 <1924324@01-18106.pusd.dom>
2025-03-12 13:19:38 -0700

    I created a 3D Array to represent the map. Need to figure out how to check which level I am in.

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.util.Queue;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class p2 {
@@ -15,27 +18,38 @@ public class p2 {
 		try {
 			File file = new File(filename);
 			Scanner scanner = new Scanner(file);
-					
+			
+			
 			int numRows = scanner.nextInt();
 			int numCols = scanner.nextInt();
 			int numRooms = scanner.nextInt();
-		
-			int rowIndex = 0;
-			//process the map!
-			while(scanner.hasNextLine()) {
-				//grab a line (one row of the map)
-				String row = scanner.nextLine();
-				
-				if(row.length() > 0) {
-					for(int i = 0; i < numCols && i < row.length(); i++) {
-						char element = row.charAt(i);
-						Tile obj = new Tile(rowIndex, i, element);
-						
-					}
-				}
-			}
 			
-		}
+			Tile[][][] tiles = new Tile[numRows][numCols][numRooms];
+			
+			int rowIndex = 0;
+			int colsIndex = 0;
+			//int roomIndex = 0;
+			//process the map!
+		 		
+		 			while(scanner.hasNextLine()) {
+					//grab a line (one row of the map)
+		 				String row = scanner.nextLine();
+				
+		 				if(row.length() > 0) {
+		 					for(int i = 0; i < numCols && i < row.length(); i++) {
+		 						char element = row.charAt(i);
+		 						Tile theTile = new Tile(rowIndex, colsIndex, element);
+		 						tiles[rowIndex][i][numRooms] = theTile;
+		 						colsIndex++; 
+						
+		 					}
+		 				}
+		 				rowIndex++;
+		 			}
+		 			
+		 		
+			
+			}
 		catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
