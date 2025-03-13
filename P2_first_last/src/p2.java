11188b5909ff2701e3e8f7020d999141dc726782
1924324 <1924324@01-18106.pusd.dom>
2025-03-13 13:53:45 -0700

    Finished the readMap with the rooms also accounted for. I also have two new methods that return the location of the W symbol;

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,4 +1,5 @@
 import java.io.File;
+
 import java.util.Queue;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -13,7 +14,7 @@ public class p2 {
 		readMap("TEST01");
 	}
 	
-	public static void readMap(String filename) {
+	public static Tile[][][] readMap(String filename) {
 		
 		try {
 			File file = new File(filename);
@@ -28,6 +29,7 @@ public class p2 {
 			
 			int rowIndex = 0;
 			int colsIndex = 0;
+			int roomsIndex = 0;
 			//int roomIndex = 0;
 			//process the map!
 		 		
@@ -39,24 +41,57 @@ public class p2 {
 		 					for(int i = 0; i < numCols && i < row.length(); i++) {
 		 						char element = row.charAt(i);
 		 						Tile theTile = new Tile(rowIndex, colsIndex, element);
-		 						tiles[rowIndex][i][numRooms] = theTile;
-		 						colsIndex++; 
+		 						tiles[rowIndex][i][roomsIndex] = theTile;
+		 						colsIndex++;
+		 						
+		 						if(element == '|') {
+		 							roomsIndex++;
+		 							rowIndex = 0;
+		 							colsIndex = 0;
+		 						}
 						
 		 					}
-		 				}
+		 				} 
 		 				rowIndex++;
 		 			}
 		 			
-		 		
+		 		return tiles;
 			
 			}
 		catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
 		
+		return null;
 		
-		
-		
+	}
+	
+	public static int findStartingRow(Tile[][][] themTiles) {
+		int row = 0;
+		for(int i = 0; i < themTiles.length; i++) {
+			for(int j = 0; j < themTiles[i].length; j++) {
+				for(int k = 0; k < themTiles[i][j].length; k++) {
+					if(themTiles[i][j][k].equals('W')) {
+						row = j;
+					}
+				}
+			}
+		}
+		return row;
+	}
+	
+	public static int findStartingColumn(Tile[][][] allThemTiles) {
+		int column = 0;
+		for(int i = 0; i < allThemTiles.length; i++) {
+			for(int j = 0; j < allThemTiles[i].length; j++) {
+				for(int k = 0; k < allThemTiles[i][j].length; k++) {
+					if(allThemTiles[i][j][k].equals('W')) {
+						column = k;
+					}
+				}
+			}
+		}
+		return column;
 	}
 
 }
